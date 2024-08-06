06c22837345f67dc6158887d6307d1a7eb9551f2
MiohitoKiri5474 <lltzpp@gmail.com>
2024-08-07 00:32:35 +0800

    feat: 🎸 implement 404 page and theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
-
-// import tailwind from "@astrojs/tailwind";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -13,4 +10,3 @@ export default defineConfig({
   site: "https://miohitokiri5474.github.io/",
   integrations: [mdx(), sitemap(), react(), image(), tailwind()],
 });
-
