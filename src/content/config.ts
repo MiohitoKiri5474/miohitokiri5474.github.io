740e3ee337486f153ece848a27f609b188ea5943
MiohitoKiri5474 <lltzpp@gmail.com>
2024-08-13 15:11:41 +0800

    fix: 🐛 the error about post type

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,8 +5,7 @@ const blog = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
+    description: z.string().optional(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
@@ -16,4 +15,5 @@ const blog = defineCollection({
 export const collections = {
   game: blog,
   code: blog,
+  blog,
 };
