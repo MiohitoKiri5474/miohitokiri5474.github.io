b5ef158ea737070aed0d93815af8c02c275eb946
MiohitoKiri5474 <lltzpp@gmail.com>
2024-08-14 04:50:47 +0800

    feat: 🎸 add underline when the blog page is active

--- src/components/BlogSelect.tsx
+++ src/components/BlogSelect.tsx
@@ -1,17 +1,34 @@
 import { Popover, PopoverButton, PopoverPanel } from "@headlessui/react";
+import { useEffect, useState } from "react";
 
-export default function Example() {
+export default function BlogSelect({ className = "" }) {
+  const [activeLink, setActiveLink] = useState("");
+
+  useEffect(() => {
+    // Determine the current path and set the active link
+    const currentPath = window.location.pathname;
+    if (currentPath.includes("/code")) {
+      setActiveLink("code");
+    } else if (currentPath.includes("/game")) {
+      setActiveLink("game");
+    }
+  }, []);
+
   return (
     <div className="flex justify-center">
       <div className="flex gap-8">
         <Popover>
-          <PopoverButton>Blogs</PopoverButton>
+          <PopoverButton
+            className={`${activeLink === "code" || activeLink === "game" ? "font-bold underline" : ""}`}
+          >
+            Blogs
+          </PopoverButton>
           <PopoverPanel
             transition
             anchor="bottom"
-            className="divide-y rounded-xltransition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:-translate-y-1 data-[closed]:opacity-0 z-30"
+            className={`backdrop-blur-lg divide-y rounded-xltransition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:-translate-y-1 data-[closed]:opacity-0 z-30 ${className}`}
           >
-            <div className="p-3 bg-orange-50 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-300 break-words leading-6 transition-colos">
+            <div className="p-3 break-words leading-6 transition-colors">
               <a
                 className="block rounded-lg py-2 px-3 transition hover:bg-white/5"
                 href="/code"
@@ -20,7 +37,7 @@ export default function Example() {
                 <p>It's time to Coding!</p>
               </a>
               <a
-                className="block rounded-lg py-2 px-3 transition hover:bg-white/5"
+                className="block rounded-lg py-2 px-3 transition hover:bg-white/5 "
                 href="/game"
               >
                 <p className="font-semibold">Game</p>
