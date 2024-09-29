ac0f342b13941f9a59e7b012128fae093a7e94ab
MiohitoKiri5474 <lltzpp@gmail.com>
2024-09-29 22:51:10 +0800

    feat: 🎸 fuck it, who cares smartphone users

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -5,7 +5,7 @@ import BlogSelect from "./BlogSelect";
 
 export default function DropdownMenu() {
   return (
-    <Menu as="div" className="relative inline-block text-left">
+    <Menu as="div" className="relative inline-block text-right">
       <div>
         <Menu.Button
           className="inline-flex justify-center rounded-md border border-zinc-400 dark:border-zinc-700 px-2 py-2 text-sm font-medium shadow-sm hover:bg-orange-200 dark:hover:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 transition-all"
@@ -23,21 +23,70 @@ export default function DropdownMenu() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <div className="py-1">
-          <a href="/" className="px-3 py-2 uppercase font-bold text-xs">
-            Home
-          </a>
-          <a href="/code" className="px-3 py-2 uppercase font-bold text-xs">
-            Code
-          </a>
-          <a href="/game" className="px-3 py-2 uppercase font-bold text-xs">
-            Game
-          </a>
-          <a href="/about" className="px-3 py-2 uppercase font-bold text-xs">
-            About
-          </a>
-          <a href="/" className="px-3 py-2 uppercase font-bold text-xs"></a>
-        </div>
+        <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right rounded-md border border-zinc-400 dark:border-zinc-700 bg-orange-50 dark:bg-zinc-800 shadow-xl ring-1 ring-black ring-opacity-5 focus:outline-none divide-y divide-zinc-400 dark:divide-zinc-700">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="/"
+                  className={`block px-4 py-2 text-sm uppercase font-bold ${
+                    active ? "bg-orange-200 dark:bg-zinc-700" : ""
+                  }`}
+                >
+                  Home
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="/code"
+                  className={`block px-4 py-2 text-sm uppercase font-bold ${
+                    active ? "bg-orange-200 dark:bg-zinc-700" : ""
+                  }`}
+                >
+                  Code
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="/game"
+                  className={`block px-4 py-2 text-sm uppercase font-bold ${
+                    active ? "bg-orange-200 dark:bg-zinc-700" : ""
+                  }`}
+                >
+                  Game
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="/about"
+                  className={`block px-4 py-2 text-sm uppercase font-bold ${
+                    active ? "bg-orange-200 dark:bg-zinc-700" : ""
+                  }`}
+                >
+                  About
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="https://github.com/MiohitoKiri5474"
+                  className={`block px-4 py-2 text-sm uppercase font-bold ${
+                    active ? "bg-orange-200 dark:bg-zinc-700" : ""
+                  }`}
+                >
+                  GitHub
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
       </Transition>
     </Menu>
   );
