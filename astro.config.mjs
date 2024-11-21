e927ea59fbfc01b8f46137277073af653e94d52d
MiohitoKiri5474 <lltzpp@gmail.com>
2024-11-21 10:26:43 +0800

    chore: update ga

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,19 @@ import tailwind from "@astrojs/tailwind";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://miohitokiri5474.github.io/",
-  integrations: [sitemap(), react(), image(), tailwind()],
+  integrations: [sitemap(), react(), image(), tailwind(), partytown()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
     extendDefaultPlugins: true,
     format: "gfm",
     remarkOptions: {
-      breaks: true,
-    },
-  },
+      breaks: true
+    }
+  }
 });
